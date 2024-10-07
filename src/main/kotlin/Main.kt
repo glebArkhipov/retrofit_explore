2242ebd0e146cbe470f99ba4d915696a66de9bc2
Gleb <gleber1995@gmail.com>
2024-10-07 22:55:23 +0200

    use parameters to run on debug tools

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,35 @@
 package com
 
 import dev.reformator.stacktracedecoroutinator.jvm.DecoroutinatorJvmApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.debug.DebugProbes
 import kotlinx.coroutines.runBlocking
 
 fun main(): Unit = runBlocking {
-    configCoroutineDebug()
+    configCoroutineDebug(
+        turnOnDecoroutinator = false,
+        turnOnDebugProbes = false
+    )
     makeApiCall()
 }
 
-
-fun configCoroutineDebug() {
-    DecoroutinatorJvmApi.install()
-//    System.setProperty(
-//        kotlinx.coroutines.DEBUG_PROPERTY_NAME,
-//        kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
-//    )
-//    DebugProbes.install()
-//    System.setProperty(
-//        kotlinx.coroutines.DEBUG_PROPERTY_NAME,
-//        kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
-//    )
+fun configCoroutineDebug(
+    turnOnDecoroutinator: Boolean,
+    turnOnDebugProbes: Boolean
+) {
+    if (turnOnDecoroutinator) {
+        DecoroutinatorJvmApi.install()
+    }
+    if (turnOnDebugProbes) {
+        System.setProperty(
+            kotlinx.coroutines.DEBUG_PROPERTY_NAME,
+            kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
+        )
+        @OptIn(ExperimentalCoroutinesApi::class)
+        DebugProbes.install()
+        System.setProperty(
+            kotlinx.coroutines.DEBUG_PROPERTY_NAME,
+            kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
+        )
+    }
 }
